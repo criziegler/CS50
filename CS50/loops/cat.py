0bd861b9cecc6d727180dd95ba38930f2032cb12
Criz <criz@crizs-macbook-pro.taile88f5a.ts.net>
2026-08-21 19:19:42 -0400

    loops hogwarts.py added

--- CS50/loops/cat.py
+++ CS50/loops/cat.py
@@ -13,3 +13,30 @@ for i in range(3):
 
 
 print("meow\n" * 3, end="")
+
+while True:
+    n = int(input("Whats n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
+
+
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("Whats n? "))
+        if n > 0:
+            break
+    return n 
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
---
+++ CS50/loops/hogwarts.py
@@ -0,0 +1,15 @@
+#students = ["Hermione", "Harry", "Ron", "Draco"]
+#houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+
+#for i in range(len(students)):
+#    print(i + 1, students[i])
+
+#dict
+students = {"Hermione": "Gryffindor", 
+            "Harry": "Gryffindor",
+            "Ron": "Gryffindor",
+            "Draco": "Slytherin",
+}
+
+for student in students:
+    print(student, students[student], sep=", ")
