ca25bb036f5f00195e562b9c1058a8e1eba8b2ac
Criz <criz@crizs-macbook-pro.taile88f5a.ts.net>
2026-08-23 19:00:39 -0400

    ProblemSet2 coke.py finished

---
+++ ProblemSet2/coke/coke.py
@@ -0,0 +1,21 @@
+def main():
+
+    print("Amount Due: 50")
+    amount_due = 50
+
+    while True:
+        n = int(input("Insert Coin: "))
+
+        if n == 25 or n == 10 or n == 5:
+            amount_due -= n
+
+            if amount_due <= 0:
+                print("Change Owed: ", abs(amount_due))
+
+                break
+
+            else: 
+                print("Amount Due: ",amount_due)
+
+        
+main()
