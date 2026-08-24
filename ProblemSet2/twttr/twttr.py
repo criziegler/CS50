61767f9c1a4931100373d9a75abfb14fa04f5bb0
Criz <criz@crizs-macbook-pro.taile88f5a.ts.net>
2026-08-24 08:31:18 -0400

    ProblemSet2 twittr.py Problem solved

---
+++ ProblemSet2/twttr/twttr.py
@@ -0,0 +1,18 @@
+
+def main():
+
+    input_word = remove_vowels(input("Input: "))
+
+    print("Output: ",input_word)
+
+def remove_vowels(word):
+    vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+    result = ""
+
+    for char in word:
+        if char not in vowels:
+            result = result + char
+        
+    return result
+
+main()
