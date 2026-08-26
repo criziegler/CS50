01555a92e4d944f3936254a0b7eadd96f47b19f3
Christian Ziegler <193838660+criziegler@users.noreply.github.com>
2026-08-26 17:12:00 -0400

    ProblemSet2 plates.py Platenumber can't start with a 0 implemented

--- ProblemSet2/plates/plates.py
+++ ProblemSet2/plates/plates.py
@@ -7,24 +7,36 @@ def main():
 
 
 def is_valid(s):
+
+
+    starts_with_two_letters = (len(s) >= 2 and s[:2].isalpha())
+
+    formatted_plate = (s.endswith() or s.isalpha())
+
     contains_special_char = any(
         char in s
         for char in "/,-._"
     )
 
-    starts_with_two_letters = (
-        len(s) >= 2 and s[:2].isalpha()
-    )
-     
-
-    if starts_with_two_letters:
-        if len(s) > 6 or contains_special_char or s.startswith("0",2,5):
+    if starts_with_two_letters and formatted_plate:
+        if len(s) > 6 or contains_special_char or s.startswith("0", 2, 6):
             return False
+        elif len(s) > 6 or contains_special_char or s.startswith("0", 3, 6) and s[:3].isalpha():
+            return False
+
+        elif len(s) > 6 or contains_special_char or s.startswith("0", 4, 6) and s[:4].isalpha():
+            return False
+
+        elif len(s) > 6 or contains_special_char or s.startswith("0", 5, 6) and s[:5].isalpha():
+            return False
+        
         else:
             return True
-
+           
     else:
         return False
+
+
         
 
 
