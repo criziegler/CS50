079cea828d94246cbc21736d95399804029b0ccf
Christian Ziegler <193838660+criziegler@users.noreply.github.com>
2026-08-26 17:29:08 -0400

    ProblemSet2 Problem plates.py finished

--- ProblemSet2/plates/plates.py
+++ ProblemSet2/plates/plates.py
@@ -8,10 +8,11 @@ def main():
 
 def is_valid(s):
 
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
     starts_with_two_letters = (len(s) >= 2 and s[:2].isalpha())
 
-    formatted_plate = (s.endswith() or s.isalpha())
+    formatted_plate = (s.endswith(tuple(numbers)) or s.isalpha() and len(s) <= 6)
 
     contains_special_char = any(
         char in s
@@ -19,15 +20,15 @@ def is_valid(s):
     )
 
     if starts_with_two_letters and formatted_plate:
-        if len(s) > 6 or contains_special_char or s.startswith("0", 2, 6):
+        if contains_special_char or s.startswith("0", 2, 6):
             return False
-        elif len(s) > 6 or contains_special_char or s.startswith("0", 3, 6) and s[:3].isalpha():
-            return False
-
-        elif len(s) > 6 or contains_special_char or s.startswith("0", 4, 6) and s[:4].isalpha():
+        elif contains_special_char or s.startswith("0", 3, 6) and s[:3].isalpha():
             return False
 
-        elif len(s) > 6 or contains_special_char or s.startswith("0", 5, 6) and s[:5].isalpha():
+        elif contains_special_char or s.startswith("0", 4, 6) and s[:4].isalpha():
+            return False
+
+        elif contains_special_char or s.startswith("0", 5, 6) and s[:5].isalpha():
             return False
         
         else:
@@ -35,9 +36,6 @@ def is_valid(s):
            
     else:
         return False
-
-
-        
 
 
 main()
