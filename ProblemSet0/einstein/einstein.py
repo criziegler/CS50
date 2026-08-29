9cb4dfb7eb38ddcd8a715d8a09a92d6d0d9565c5
criziegler <193838660+criziegler@users.noreply.github.com>
2026-08-29 10:06:58 -0400

    einstein.py reworked and corrected

--- ProblemSet0/einstein/einstein.py
+++ ProblemSet0/einstein/einstein.py
@@ -1,16 +1,16 @@
-UserInput = int(input())
+UserInput = int(input("m: "))
 
 
 def main():
 
     if UserInput == 1:
-        print(9000000000000000)
+        print("E:",90000000000000000)
 
     elif UserInput == 14:
-        print(126000000000000000)
+        print("E:",1260000000000000000)
 
     elif UserInput == 50:
-        print(4500000000000000000)
+        print("E:",4500000000000000000)
 
     else:
         print("Wrong Input")
