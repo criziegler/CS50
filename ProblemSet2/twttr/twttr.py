2d3f417521811a4f56377f145eb8fc1facb2505e
criziegler <193838660+criziegler@users.noreply.github.com>
2026-08-31 16:47:32 -0400

    changed space after Output to pass submission

--- ProblemSet2/twttr/twttr.py
+++ ProblemSet2/twttr/twttr.py
@@ -3,7 +3,7 @@ def main():
 
     input_word = remove_vowels(input("Input: "))
 
-    print("Output: ",input_word)
+    print("Output:",input_word)
 
 def remove_vowels(word):
     vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
