b5e191e60da4cb1992010d940c7b825cf2611934
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-03 20:41:29 -0400

    gorcery.py ProblemSet3 finished, passed check50 and submitted

--- ProblemSet3/grocery/grocery.py
+++ ProblemSet3/grocery/grocery.py
@@ -1,22 +1,40 @@
 def main():
 
     grocery = []
-    lower_list = []
-    upper_list = []
 
     while True:
+
         try:
-            item = input()
-            grocery.append(item)
+            items = input()
+            grocery.append(items)
+            upper_list = [item.upper() for item in grocery]
+            upper_list.sort() 
+
+            counts = {word: items.count(word) for word in upper_list}
+
+            for word in upper_list:
+                if word in counts:
+                    counts[word] += 1
+                else:
+                    counts[word] = 1
 
         except KeyboardInterrupt:
-            print(grocery)
-            return
+            print("\n")
+            if grocery == []:
+                return
+            else:
+                for key,value in counts.items():
+                    print(f"{value} {key}")
+                return       
 
         except EOFError:
-            print(grocery)
-            return
-    
-
+            print("\n")
+            if grocery == []:
+                return
+            else:
+                for key,value in counts.items():
+                    print(f"{value} {key}")
+                return
+        
 
 main()
