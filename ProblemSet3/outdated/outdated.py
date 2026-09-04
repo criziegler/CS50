0a8b672bfe58f83ba40b2c55c8a7401169f6f0e5
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-04 17:03:42 -0400

    base structure problemset3 outdated.py implemented

--- ProblemSet3/outdated/outdated.py
+++ ProblemSet3/outdated/outdated.py
@@ -1,24 +1,42 @@
-months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
-
 def main():
-    get_input("Date: ")
 
+    months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
 
+    while True:
 
-def get_input(prompt):
-    ...
+        try:
+            date_input = input("Date: ")
+            start_with_digit = (date_input[:1].isdigit())
+
+            if start_with_digit:
+                x, y, z = date_input.split("/")
+                print(f"{z}-{x}-{y}")
+                return
+
+            elif date_input.startswith(tuple(months)):
+                print("yes")
+            
+            else:
+                pass 
+
+        except KeyboardInterrupt:
+            print("\n")
+            return
+
+        except KeyError:
+            pass
 
 main()
