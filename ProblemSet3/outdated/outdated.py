0fd11d0b32334a3139ce048455030172bb9ba4f2
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-04 17:18:26 -0400

    outdated.py format 8/9/2023 implemented

--- ProblemSet3/outdated/outdated.py
+++ ProblemSet3/outdated/outdated.py
@@ -23,9 +23,21 @@ def main():
 
             if start_with_digit:
                 x, y, z = date_input.split("/")
-                print(f"{z}-{x}-{y}")
-                return
-
+                x = int(x)
+                y = int(y)
+                if x <= 9 and y >= 10:
+                    print(f"{z}-0{x}-{y}")
+                    return
+                elif y <= 9 and x >= 10:
+                    print(f"{z}-{x}-0{y}")
+                    return
+                elif x <= 9 and y <= 9:
+                    print(f"{z}-0{x}-0{y}")
+                    return
+                else:
+                    print(f"{z}-{x}-{y}")
+                    return
+                
             elif date_input.startswith(tuple(months)):
                 print("yes")
             
