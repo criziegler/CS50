74760a78a8606469a097d02110afa843c6c3faf2
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-06 13:56:18 -0400

    reworked outdated.py now it passes check50

--- ProblemSet3/outdated/outdated.py
+++ ProblemSet3/outdated/outdated.py
@@ -48,41 +48,68 @@ def main():
                         else:
                             print(f"{z}-{x}-{y}")
                             return
+                
                 else:
                     pass
+
+            elif date_input.startswith(" ") and date_input.endswith(" "):
+                date_input.strip()
+                d, e, f = date_input.split("/")
+                d = int(d)
+                e = int(e)
+                f = f.strip()
+                if d <= 12 and e <= 31:
+                    if d <= 9 and e <= 9:
+                        print(f"{f}-0{d}-0{e}")
+                        return
+                    elif d <= 9 and e >= 10:
+                        print(f"{f}-0{d}-{e}")
+                        return
+                    elif d >= 10 and e <= 9:
+                        print(f"{f}-{d}-0{e}")
+                        return
+                    else:
+                        print(f"{f}-{d}-{e}")
+                        return
+
+                else:
+                    pass        
                     
-            elif starts_with_month:
+            elif starts_with_month and date_input.index(","):
                 a, b, c = date_input.split(" ")
                 b_formatted = b.strip(",")
                 b_formatted = int(b_formatted)
 
-                if any(months[i] == a for i in months_with_30_days) and b_formatted > 30:
-                    pass
-                elif a == "February" and b_formatted > 29:
-                    pass
-                else:
-                    months_dict = dict(zip(months, months_value))
-
-                    if a in months[:9] and b_formatted >= 10:
-                        print(f"{c}-0{months_dict[a]}-{b_formatted}")
-                        return
-                    elif b_formatted <= 9 and a in months[:10]:
-                        print(f"{c}-{months_dict[a]}-0{b_formatted}")
-                        return
-                    elif a in months[:9] and b_formatted <= 9:
-                        print(f"{c}-0{months_dict[a]}-0{b_formatted}")
-                        return
+                if starts_with_month and b_formatted <= 31:
+                    if any(months[i] == a for i in months_with_30_days) and b_formatted > 30:
+                        pass
+                    elif a == "February" and b_formatted > 29:
+                        pass
                     else:
-                        print(f"{c}-{months_dict[a]}-{b_formatted}")
-                        return
-            else:
-                pass 
+                        months_dict = dict(zip(months, months_value))
 
+                        if a in months[:9] and b_formatted >= 10:
+                            print(f"{c}-0{months_dict[a]}-{b_formatted}")
+                            return
+                        elif a in months[9:12] and b_formatted <= 9 :
+                            print(f"{c}-{months_dict[a]}-0{b_formatted}")
+                            return
+                        elif a in months[:9] and b_formatted <= 9:
+                            print(f"{c}-0{months_dict[a]}-0{b_formatted}")
+                            return
+                        else:
+                            print(f"{c}-{months_dict[a]}-{b_formatted}")
+                            return
+                
+            else:
+                return 
+
+        except ValueError:
+            ...
+        
         except KeyboardInterrupt:
             print("\n")
             return
 
-        except KeyError:
-            pass
 
 main()
