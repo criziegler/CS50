667b7b8f31c6daec291b5658ca61d924aa229321
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-06 17:10:55 -0400

    finished lecture Libraries

--- Lecture/Libraries/say.py
+++ Lecture/Libraries/say.py
@@ -1,6 +1,11 @@
 import cowsay
 import sys
 
+from sayings import hello
+
 if len(sys.argv) == 2:
     cowsay.cow("hello, " + sys.argv[1])
-    cowsay.trex("hello, " + sys.argv[1])
+    # cowsay.trex("hello, " + sys.argv[1])
+
+if len(sys.argv) == 2:
+    hello(sys.argv[1])
