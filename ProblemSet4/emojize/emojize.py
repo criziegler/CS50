4833df301323b88479f51c1181f13de97d1e86aa
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-09 01:02:14 +0200

    changed emojize.py to pass check50

--- ProblemSet4/emojize/emojize.py
+++ ProblemSet4/emojize/emojize.py
@@ -1,20 +1,10 @@
 import emoji
 
-emojilist = [
-    ":1st_place_medal:",
-    ":money_bag:",
-    ":smile_cat:",
-]
-
 def main():
 
     user_input = input("Input: ")
-    try:
-        if user_input in emojilist:
-            print(emoji.emojize(user_input))
-            return
-    except:
-        pass
+
+    print(emoji.emojize(user_input, language="alias"))
 
 
 main()
