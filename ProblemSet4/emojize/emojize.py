ca2cad24cb842f1d779c6c4ef4e6dfbbe1fa6fd0
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-09 00:20:44 +0200

    emojize.py updated

--- ProblemSet4/emojize/emojize.py
+++ ProblemSet4/emojize/emojize.py
@@ -1,8 +1,20 @@
 import emoji
 
+emojilist = [
+    ":1st_place_medal:",
+    ":money_bag:",
+    ":smile_cat:",
+]
+
 def main():
 
-    ...
+    user_input = input("Input: ")
+    try:
+        if user_input in emojilist:
+            print(emoji.emojize(user_input))
+            return
+    except:
+        pass
 
 
 main()
