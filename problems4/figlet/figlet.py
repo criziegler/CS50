e23d7dffafaf8cdf6eafee39fad07d2143359abd
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-10 01:33:48 +0200

    changed randmo stuff in figlet.py to figure the package pyfiglet out problem not solved just pushed so i can work from another pc on it

--- problems4/figlet/figlet.py
+++ problems4/figlet/figlet.py
@@ -1,21 +1,23 @@
-from pyfiglet import Figlet
 import sys
 import random
+from pyfiglet import Figlet
 
-figlet = Figlet()
 
 def main():
 
-
+    figlet = Figlet()
     user_input = input("Input: ")
 
+    if sys.argv[2] in figlet.getFonts():
+        x, y = sys.argv[2].split(" ")
+        print (x,y)
+        print(figlet.renderText(user_input))
 
-    print(figlet.renderText(user_input))
+    elif figlet.setFont(font="slant"):
+        print(figlet.renderText(user_input, font="slant"))
+    else:
+        ...
 
-
-
-    # f = pyfiglet.figlet_format("text to render",font="slant")
-    # print(f)
 
 
 main()
