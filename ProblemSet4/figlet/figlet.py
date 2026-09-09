6f89ee36178d87680fb0718cffdb54b182740e84
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-10 00:32:15 +0200

    updated figlet.py isn't working yet just wanna clear the pipeline

--- ProblemSet4/figlet/figlet.py
+++ ProblemSet4/figlet/figlet.py
@@ -1,5 +1,21 @@
 import pyfiglet
+import sys
 
-f = pyfiglet.figlet_format("text to render",font="slant")
-print(f)
 
+def main():
+
+    user_input = input("Input: ")
+
+    if len(sys.argv) == 2:
+        print(pyfiglet.figlet_format(user_input, sys.argv[2]))
+
+    else:
+        print(pyfiglet.figlet_format(user_input))
+
+
+
+    # f = pyfiglet.figlet_format("text to render",font="slant")
+    # print(f)
+
+
+main()
