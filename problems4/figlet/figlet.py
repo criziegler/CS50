94e34f0cdc2185f5f1a0ccd191a6d028e5780194
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-11 01:20:07 +0200

    figlet.py reworked and passed check50

--- problems4/figlet/figlet.py
+++ problems4/figlet/figlet.py
@@ -7,19 +7,31 @@ def main():
 
     figlet = Figlet()
 
-    if sys.argv[1] == "-f" or sys.argv[1] == "--font":
-        if len(sys.argv) < 2:
+    try:
+
+        if sys.argv[1] == "-f" or sys.argv[1] == "--font":
+            if len(sys.argv) < 2:
+                sys.exit("Invalid usage")
+
+            figlet.getFonts()
+            fonts = sys.argv[2:4]
+            font = fonts[0]
+            print(font)
+            figlet.setFont(font=font)
+            text = input("Input: ")
+            output = figlet.renderText(text)
+            print("Output: ", output)
+
+        else:
             sys.exit("Invalid usage")
 
-        figlet.getFonts()
-        fonts = sys.argv[2:4]
-        font = fonts[0]
-        print(font)
-        figlet.setFont(font=font)
-        text = input("Input: ")
-        output = figlet.renderText(text)
-        print("Output: ", output)
-
+    except IndexError:
+        text_random = input("Input: ")
+        font_list = figlet.getFonts()
+        font_random = random.choice(font_list)
+        figlet.setFont(font=font_random)
+        output_random = figlet.renderText(text_random)
+        print("Output: ", output_random)
 
 
 
