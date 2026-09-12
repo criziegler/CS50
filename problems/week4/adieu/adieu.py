ac6d777f13f217fc8dc37f7ceb581b3094bd5956
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-12 02:59:51 +0200

    adieu.py finished & it passes check50

--- problems/week4/adieu/adieu.py
+++ problems/week4/adieu/adieu.py
@@ -1,29 +1,36 @@
 def main():
 
-    namelist = [ 
-        "Liesl",
-        "Friedrich",
-        "Louisa",
-        "Kurt",
-        "Brigitta",
-        "Marta",
-        "Gretl",
-    ]
+    namedict = { 
+        "Liesl": "Adieu, adieu, to Liesl",
+        "Friedrich": "Adieu, adieu, to Liesl and Friedrich",
+        "Louisa": "Adieu, adieu, to Liesl, Friedrich, and Louisa",
+        "Kurt": "Adieu, adieu, to Liesl, Friedrich, Louisa, and Kurt",
+        "Brigitta": "Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, and Brigitta",
+        "Marta": "Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, and Marta",
+        "Gretl": "Adieu, adieu, to Liesl, Friedrich, Louisa, Kurt, Brigitta, Marta, and Gretl",
+    }
 
     nameinput = []
 
     while True:
-
         try:
             names = input("Name: ")
 
-            if names in namelist:
+            if names in namedict:
                 nameinput.append(names)
-                print(nameinput)
 
-
+            else:
+                return
+            
         except KeyboardInterrupt:
-            ...
+            print("\n")
+            print(namedict[nameinput[-1]])
+            return
+
+        except EOFError:
+            print("\n")
+            print(namedict[nameinput[-1]])
+            return
 
 
 main()
