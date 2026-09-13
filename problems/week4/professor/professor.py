d167ab149d18070b24bc6a3b32118ffe0c20460e
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-13 02:35:40 +0200

    started professor.py

--- problems/week4/professor/professor.py
+++ problems/week4/professor/professor.py
@@ -1,16 +1,34 @@
 import random
 
 def main():
-    get_level("Level: ")
+
+    while True:
+        lev = get_level("Level: ")
 
 
-def get_level():
-    ...
+def get_level(prompt):
 
+    level_list = [1, 2, 3]
+
+    while True:
+        try:
+            n = input(prompt)
+            n = int(n)
+
+            if n in level_list:
+                print("yes")
+            else:
+                pass
+
+        except KeyboardInterrupt:
+            return
+
+        except EOFError:
+            return
 
 def generate_integer(level):
     ...
 
 
 if __name__ == "__main__":
-    main()
+    main()  
