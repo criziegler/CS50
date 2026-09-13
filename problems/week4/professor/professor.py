4844622031411c811f3b96923b965df2feea0f1d
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-13 23:32:32 +0200

    reworked professor.py | get_level checks if level is 1,2 or 3 and then returns level

--- problems/week4/professor/professor.py
+++ problems/week4/professor/professor.py
@@ -2,45 +2,53 @@ import random
 
 def main():
 
-    user_input = get_level("Level: ")
+    try:
+         
+        m = get_level("Level: ")
+        score = 0
+        x0 = generate_integer(m)
+        y0 = generate_integer(m)
 
+        attempts0 = 0
+    
+        while attempts0 < 3:
+            math0 = input(f"{x0} + {y0} = ")
+            math0 = int(math0)
+            result0 = x0 + y0
+    
+            if result0 == math0:
+                score = score + 1
+                print("Score:", score)
+                break
+    
+            elif result0 != math0:
+                attempts0 = attempts0 + 1
+                print("EEE")
+                pass    
+                        
+        if attempts0 == 3:
+            print(f"{x0} + {y0} =", result0)
+
+    except ValueError:
+         return
+
+    except KeyboardInterrupt:
+         return
+
+    except EOFError:
+        return
 
 def get_level(prompt):
 
     while True:
-        try:
             n = input(prompt)
             n = int(n)
-            score = 0
-            x0 = generate_integer(n)
-            y0 = generate_integer(n)
 
             if n == 1 or n == 2 or n == 3:
-                attempts0 = 0
+                return n
 
-                while attempts0 < 3:
-                    math0 = input(f"{x0} + {y0} = ")
-                    math0 = int(math0)
-                    result0 = x0 + y0
-
-                    if result0 == math0:
-                        score = score + 1
-                        print("Score:", score)
-                        break
-
-                    elif result0 != math0:
-                            attempts0 = attempts0 + 1
-                            print("EEE")
-                            pass    
-                    
-                if attempts0 == 3:
-                    print(f"{x0} + {y0} =", result0)
-
-        except KeyboardInterrupt:
-            return
-
-        except EOFError:
-            return
+            else:
+                return n
 
 def generate_integer(level):
 
