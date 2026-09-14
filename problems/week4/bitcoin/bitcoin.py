ba8cf0dc47c6833d9bb711d8f793c21fb13881da
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-15 01:49:16 +0200

    problem bitcoin.py

--- problems/week4/bitcoin/bitcoin.py
+++ problems/week4/bitcoin/bitcoin.py
@@ -1,6 +1,29 @@
+import json
 import requests
+import sys
+
 
 try:
-    ...
+
+    number = sys.argv[1]
+    n = float(number)
+
+
+
+    key = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=apikey")
+
+    text = key.json()
+
+    bitcoin_dict = text["data"]
+
+    print(bitcoin_dict["priceUsd"])
+
+
 except requests.RequestException:
     ...
+
+except ValueError:
+    sys.exit("Command-line argument is not a number")
+
+except IndexError:
+    sys.exit("Missing command-line argument")
