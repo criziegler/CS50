a7c8a0f6750b5fb1bd3fd861995c5a1948ff3380
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-15 02:09:31 +0200

    problem bitcoin.py solved | passed check50

--- problems/week4/bitcoin/bitcoin.py
+++ problems/week4/bitcoin/bitcoin.py
@@ -8,15 +8,16 @@ try:
     number = sys.argv[1]
     n = float(number)
 
-
-
-    key = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=apikey")
+    key = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=your_key")
 
     text = key.json()
 
     bitcoin_dict = text["data"]
+    bitcoin = bitcoin_dict["priceUsd"]
+    bitcoin = float(bitcoin)
 
-    print(bitcoin_dict["priceUsd"])
+    amount = bitcoin * n
+    print(f"${amount:,.4f}")
 
 
 except requests.RequestException:
