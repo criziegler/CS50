50b1463e42a6adcdf9eaca99a8ceac76fb90bb95
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-17 23:00:03 +0200

    lecture examples

--- lecture/file/students.py
+++ lecture/file/students.py
@@ -1,6 +1,31 @@
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         print(f"{name}, {house}")
+#         # row = line.rstrip().split(",")
+        # print(f"{row[0]} is in {row[1]}")
+
+students = []
+
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        print(f"{name}, {house}")
-        # row = line.rstrip().split(",")
-        # print(f"{row[0]} is in {row[1]}")
+        student = {"name": name, "house": house}
+        # student ["name"] = name
+        # student ["house"] = house
+        students.append(student)
+#         students.appen(f"{name} is in {house}")
+
+# for student in sorted(students):
+#     print(student)
+
+def get_name(student):
+    return student["name"]
+
+def get_house(student):
+    return student["house"]
+
+# for student in sorted(students, key=get_name, reverse=True):
+#     print(f"{student["name"]} is in {student["house"]}")
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student["name"]} is in {student["house"]}")
