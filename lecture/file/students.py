ff9d39dfd7de79e9816800819b007ca8562732b4
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-17 23:39:31 +0200

    lecture examples

--- lecture/file/students.py
+++ lecture/file/students.py
@@ -11,9 +11,14 @@ students = []
 
 
 with open("students.csv") as file:
-    reader = csv.reader(file)
-    for name, home in reader:
-        students.append({"name": name, "home": home})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row["home"]})
+    
+
+    # reader = csv.reader(file)
+    # for name, home in reader:
+    #     students.append({"name": name, "home": home})
 
 
 for student in sorted(students, key=lambda student: student["name"]):
