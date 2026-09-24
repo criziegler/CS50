1bb0ca2ec0face8ba9258b939d8e07b8f173f7bc
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-24 02:31:21 +0200

    problem scourgify.py updated ready for check50

--- problems/week6/scourgify/scourgify.py
+++ problems/week6/scourgify/scourgify.py
@@ -17,13 +17,15 @@ else:
 
 
         with open(sys.argv[2], "w") as f:
+
+            writer = csv.DictWriter(f, fieldnames=["name","house"])
+            writer.writeheader()
+
             for student in sorted(students, key=lambda student: student["name"].split(",")[1]):
                 last_name, first_name = student["name"].split(",", 1)
                 f_name = f"{first_name}, {last_name}"
-                print(f"{f_name}")
+
                 formatted_dict = {"name": f_name, "house": student["house"]}
-            
-                writer = csv.DictWriter(f, fieldnames=["name","house"])
                 writer.writerow(formatted_dict)
 
 
