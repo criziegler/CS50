5fd13ae99046356f1179e615c216de466e5ca146
criziegler <193838660+criziegler@users.noreply.github.com>
2026-09-25 03:40:28 +0200

    found issue with shirt.py fixed it and now it passed check50

--- problems/week6/shirt/shirt.py
+++ problems/week6/shirt/shirt.py
@@ -1,36 +1,41 @@
 import sys
 from PIL import Image, ImageOps
 
-try:
+def main():
 
-    if len(sys.argv) == 1 or len(sys.argv) == 2:
-        sys.exit("Too few command-line arguments")
+    try:
 
-    elif len(sys.argv) == 4:
-        sys.exit("Too many command-line arguments")
+        if len(sys.argv) == 1 or len(sys.argv) == 2:
+            sys.exit("Too few command-line arguments")
 
-    else:
-
-        image1 = sys.argv[1].lower()
-        image2 = sys.argv[2].lower()
-
-        if (image1.endswith(".jpg") and image2.endswith(".jpg") 
-            or image1.endswith(".jpeg") and image2.endswith(".jpeg") 
-            or image1.endswith(".png") and image2.endswith(".png")):
-
-            shirt = Image.open("shirt.png")
-            
-            image = Image.open(sys.argv[1])
-            image_re = ImageOps.fit(image, shirt.size)
-
-            after = Image.open(sys.argv[2])
-            after.paste(image_re)
-            after.paste(shirt, shirt)
-            
-            after.save(sys.argv[2])
+        elif len(sys.argv) >= 4:
+            sys.exit("Too many command-line arguments")
 
         else:
-            sys.exit("Input and output have different extensions")
 
-except FileNotFoundError:
-    sys.exit("Input does not exist")
+            image1 = sys.argv[1].lower()
+            image2 = sys.argv[2].lower()
+
+            if (image1.endswith(".jpg") and image2.endswith(".jpg") 
+                or image1.endswith(".jpeg") and image2.endswith(".jpeg") 
+                or image1.endswith(".png") and image2.endswith(".png")):
+
+                shirt = Image.open("shirt.png")
+            
+                image = Image.open(sys.argv[1])
+                image_re = ImageOps.fit(image, shirt.size)
+
+                after = Image.new("RGB", shirt.size)
+                after.paste(image_re)
+                after.paste(shirt, shirt)
+            
+                after.save(sys.argv[2])
+
+            else:
+                sys.exit("Input and output have different extensions")
+
+    except FileNotFoundError:
+        sys.exit("Input does not exist")
+
+
+main()
